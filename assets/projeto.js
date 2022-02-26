d4d913460060314d3a5dd66bcfbb09f69b938918
Jonatan Hartmann <jonatanhartamann@gmail.com>
2022-02-26 15:44:30 -0300

    Implementando função de pesquisa por nome de task

--- assets/projeto.js
+++ assets/projeto.js
@@ -36,6 +36,8 @@ window.onload = function(){
             setTimeout(() => {  checkTask(this.id); }, 700);
         })
     );
+
+    if(document.getElementById("formSearch") != null){document.getElementById("formSearch").addEventListener("reset", cancelSearch, false);} //Pesquisa de itens
 }
 function checkTask(id){
     var idForm = "check"+id.replace("cbx", "");
@@ -89,4 +91,69 @@ function editarTask(idTask){
     document.querySelectorAll(select).forEach((el) =>
         el.value = idTask
     );
+}
+
+if(document.getElementById('search') != null){ //Funções para pesquisar itens com javascript
+    //setup before functions
+    let typingTimer;                //timer identifier
+    let doneTypingInterval = 500;  //time in ms (5 seconds)
+    let myInput = document.getElementById('search');
+
+    //on keyup, start the countdown
+    myInput.addEventListener('keyup', () => {
+        var list = document.querySelectorAll( '.inputSearch' );
+        Array.prototype.forEach.call(list, function (el) {
+            var father = el.parentNode;
+            var grandpa = father.parentNode;
+            var grandpafather = grandpa.parentNode;
+            var grandpagrandpa = grandpafather.parentNode;
+            grandpagrandpa.style.display = "block";
+        });
+        document.getElementById("resetSearch").style.visibility = "visible";
+        clearTimeout(typingTimer);
+        if (myInput.value) {
+            typingTimer = setTimeout(doneTyping, doneTypingInterval);
+        }
+    });
+
+    function cancelSearch() {
+        document.getElementById("hasNot").style.visibility = "hidden";
+        var list = document.querySelectorAll( '.inputSearch' );
+        Array.prototype.forEach.call(list, function (el) {
+            el.style.display = "block";
+            var father = el.parentNode;
+            father.style.display = "block";
+            var grandpa = father.parentNode;
+            grandpa.style.display = "block";
+            var grandpafather = grandpa.parentNode;
+            grandpafather.style.display = "block";
+            var grandpagrandpa = grandpafather.parentNode;
+            grandpagrandpa.style.display = "block";
+        });
+    }
+
+    //user is "finished typing," do something
+    function doneTyping () {
+        document.getElementById('detail-tasks-checked').open = "true";
+        var text = myInput.value;
+        if(text.length > 0){
+            var verif = false;
+            var list = document.querySelectorAll( '.inputSearch' );
+            Array.prototype.forEach.call(list, function (el) {
+                if(el.innerHTML.toLowerCase().includes(text.toLowerCase()) == false){
+                    var father = el.parentNode;
+                    var grandpa = father.parentNode;
+                    var grandpafather = grandpa.parentNode;
+                    var grandpagrandpa = grandpafather.parentNode;
+                    grandpagrandpa.style.display = "none";
+                } else{
+                    verif = true;
+                }
+            });
+
+            if(verif == false){
+                document.getElementById("hasNot").style.visibility = "visible";
+            }
+        }
+    }
 }
